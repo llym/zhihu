11b8050d73b33b41242e69eccf3b68d6500d8e55
LJY <121682493@qq.com>
2019-07-04 10:04:06 +0800

    ljy 可通过personalHomePage.do跳转到个人主页

---
+++ src/com/jy/controller/FunctionController.java
@@ -0,0 +1,15 @@
+package com.jy.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+@Controller
+public class FunctionController {
+	
+	@RequestMapping("personalHomePage.do")
+	public ModelAndView personal() {
+		ModelAndView mav = new ModelAndView("MyPage");
+		return mav;
+	}
+}
